2cd756b7fde7f9e4a0158f995e05326348a4e759
ignacio <nachomariano19060@gmail.com>
2023-06-13 19:53:14 -0300

    arregle algo de las solicitudes, Todavia crashea para solicitar turno

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -154,6 +154,25 @@ router.post('/nuevot',(req, res) => {
      }) 
   });
 
+  router.post('/solicitarTurno',(req, res) => {
+    const tipo = req.body.tipoTurno;
+    const servicio = req.body.tipoServicio;
+    const usuario = req.body.usuario;
+    req.getConnection((err,conn) =>{
+    sql = "INSERT INTO `solicitarturno`(`tipoTurno`, `tipoServicio`,`usuario`) VALUES (?,?,?)"
+    conn.query(sql, [tipo,servicio,usuario], (err, result) => {
+      if (err) {
+        res.send('hubo un error')
+        res.redirect('/');
+      } else {
+        res.redirect('/'); 
+      }
+    });  
+     }) 
+  });
+  
+
+
 
 router.get('/add_mascota',(req, res) => {
     var user= req.session.mi_sesion
